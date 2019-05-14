e139612335e3ba5e58f314e5653db86667d07401
Deividas Januska <dj9909@gmail.com>
2019-05-14 21:02:07 +0300

    1 uzduotis

--- Vector.h
+++ Vector.h
@@ -4,22 +4,25 @@
 #include <stdexcept>
 
 template <typename T>
-class Vector {
+class Vector
+{
 public:
 	using value_type = T;
-	using reference = T&;
-	using const_reference = const T&;
+	using reference = T &;
+	using const_reference = const T &;
 	using size_type = std::size_t;
-	using pointer = T*;
-	using iterator = T*;
-	using const_iterator = const T*;
+	using pointer = T *;
+	using iterator = T *;
+	using const_iterator = const T *;
 
-//////////////////////CONSTRUCTORS//////////////////////
+	//////////////////////CONSTRUCTORS//////////////////////
 	//default
 	Vector() : size_{0}, capacity_{0}, elem_{new value_type[0]} {}
 	//fill with specified value
-	Vector(size_type s, value_type val) {
-		if (s < 0) throw std::out_of_range {"class Vector cannot contain negative amount of values"};
+	Vector(size_type s, value_type val)
+	{
+		if (s < 0)
+			throw std::out_of_range{"class Vector cannot contain negative amount of values"};
 		size_ = s;
 		capacity_ = size_;
 		elem_ = new value_type[capacity_];
@@ -27,45 +30,45 @@ public:
 		std::fill_n(elem_, s, val);
 	}
 	//copy
-	Vector(const Vector& vec) :
-		size_{vec.size_},
-		capacity_{vec.size_},
-		elem_{new value_type[vec.size_]}
+	Vector(const Vector &vec) : size_{vec.size_},
+								capacity_{vec.size_},
+								elem_{new value_type[vec.size_]}
 	{
 		for (int i = 0; i != size_; ++i)
 			elem_[i] = vec.elem_[i];
 	}
 	//initializer list
-	Vector(std::initializer_list<value_type> il) :
-		size_{il.size()},
-		capacity_{il.size()},
-		elem_{new value_type[il.size()]}
+	Vector(std::initializer_list<value_type> il) : size_{il.size()},
+												   capacity_{il.size()},
+												   elem_{new value_type[il.size()]}
 	{
 		std::copy(il.begin(), il.end(), elem_);
 	}
 	//move
-	Vector(Vector&& vec) :
-		size_{vec.size_},
-		capacity_{vec.size_},
-		elem_{vec.elem_}
+	Vector(Vector &&vec) : size_{vec.size_},
+						   capacity_{vec.size_},
+						   elem_{vec.elem_}
 	{
 		vec.elem_ = nullptr;
 		vec.size_ = 0;
 		vec.capacity_ = 0;
 	}
-	~Vector() {	delete[] elem_; }
+	~Vector() { delete[] elem_; }
 
-//////////////////////METHODS//////////////////////
+	//////////////////////METHODS//////////////////////
 	//assign "size" amount of "values"
-	void assign(size_type size, const_reference value) {
-		if (size < 0) throw std::out_of_range {"Vector::assign"};
+	void assign(size_type size, const_reference value)
+	{
+		if (size < 0)
+			throw std::out_of_range{"Vector::assign"};
 		delete[] elem_;
 		size_ = size;
 		capacity_ = size_;
 		elem_ = new value_type[size_];
 		std::fill_n(elem_, size_, value);
 	}
-	void assign(std::initializer_list<value_type> il) {
+	void assign(std::initializer_list<value_type> il)
+	{
 		delete[] elem_;
 		size_ = il.size();
 		capacity_ = size_;
@@ -74,12 +77,16 @@ public:
 	}
 
 	//element at specified position
-	reference at(size_type pos) {
-		if (pos < 0 || size() < pos) throw std::out_of_range {"Vector::at_ref"};
+	reference at(size_type pos)
+	{
+		if (pos < 0 || size() < pos)
+			throw std::out_of_range{"Vector::at_ref"};
 		return elem_[pos];
 	}
-	const_reference at(size_type pos) const {
-		if (pos < 0 || size() < pos) throw std::out_of_range {"Vector::at_c_ref"};
+	const_reference at(size_type pos) const
+	{
+		if (pos < 0 || size() < pos)
+			throw std::out_of_range{"Vector::at_c_ref"};
 		return elem_[pos];
 	}
 
@@ -88,7 +95,8 @@ public:
 	const_reference back() const { return elem_[size() - 1]; }
 
 	//iterator pointing to the first element of the container
-	iterator begin() {
+	iterator begin()
+	{
 		return &elem_[0];
 	}
 
@@ -96,8 +104,10 @@ public:
 	size_type capacity() const { return capacity_; }
 
 	//size_ = 0, capacity stays the same
-	void clear() noexcept {
-		for (auto i = 0; i < size_; i++) {
+	void clear() noexcept
+	{
+		for (auto i = 0; i < size_; i++)
+		{
 			elem_[i].~value_type();
 		}
 		size_ = 0;
@@ -108,20 +118,24 @@ public:
 	const pointer data() const noexcept { return elem_; }
 
 	//emplaces new object at specified pos and returns iterator, pointing to that place
-	template< class... Args >
-	iterator emplace( const_iterator pos, Args&&... args ){
+	template <class... Args>
+	iterator emplace(const_iterator pos, Args &&... args)
+	{
 		iterator tmp = begin();
 		size_type i = 0;
-		while (tmp != pos) {
+		while (tmp != pos)
+		{
 			i++;
 			tmp++;
 		}
 		tmp = nullptr;
 
-		if (size_ + 1 > capacity_) {
+		if (size_ + 1 > capacity_)
+		{
 			reserve(capacity_ * 2);
 		}
-		for (auto j = size_; j > i; j--) {
+		for (auto j = size_; j > i; j--)
+		{
 			elem_[j] = elem_[j - 1];
 		}
 		elem_[i] = value_type(std::forward<Ts>(args)...);
@@ -131,10 +145,10 @@ public:
 		return &elem_[i];
 	}
 
-
 	//emplaces new object back to the array
 	template <typename... Ts>
-	void emplace_back(Ts&&... args) {
+	void emplace_back(Ts &&... args)
+	{
 		push_back(value_type(std::forward<Ts>(args)...));
 	}
 
@@ -142,26 +156,30 @@ public:
 	bool empty() const { return size_ == 0; }
 
 	//next address after the last element
-	iterator end() {return elem_ + size_;}
+	iterator end() { return elem_ + size_; }
 
 	//reference to the first element
 	reference front() { return elem_[0]; }
 	const_reference front() const { return elem_[0]; }
 
 	//inserts new object at specified pos and returns iterator, pointing to that place
-	iterator insert(iterator pos, const_reference value) {
+	iterator insert(iterator pos, const_reference value)
+	{
 		iterator tmp = begin();
 		size_type i = 0;
-		while (tmp != pos) {
+		while (tmp != pos)
+		{
 			i++;
 			tmp++;
 		}
 		tmp = nullptr;
 
-		if (size_ + 1 > capacity_) {
+		if (size_ + 1 > capacity_)
+		{
 			reserve(capacity_ * 2);
 		}
-		for (auto j = size_; j > i; j--) {
+		for (auto j = size_; j > i; j--)
+		{
 			elem_[j] = elem_[j - 1];
 		}
 		elem_[i] = value;
@@ -172,34 +190,46 @@ public:
 	}
 
 	//removes last element from the container
-	void pop_back() {
-		if (size_ > 0) {
-			delete &elem_[--size_];
+	void pop_back()
+	{
+		if (size_ > 0)
+		{
+			elem_[--size_].~value_type();
 		}
 	}
 
 	//pushes a new element to the back of the container
-	void push_back(const_reference value ) {
-		if (capacity_ == 0) {
+	void push_back(const_reference value)
+	{
+		if (capacity_ == 0)
+		{
 			elem_ = new value_type[1];
 			elem_[0] = value;
 			capacity_ = 1;
 			size_ = 1;
-		} else {
-			if (!(size_ < capacity_)) {
+		}
+		else
+		{
+			if (!(size_ < capacity_))
+			{
 				reserve(capacity_ * 2);
 			}
 			elem_[size_++] = value;
 		}
 	}
-	void push_back(T&& value ) {
-		if (capacity_ == 0) {
+	void push_back(T &&value)
+	{
+		if (capacity_ == 0)
+		{
 			elem_ = new value_type[1];
 			elem_[0] = value;
 			capacity_ = 1;
 			size_ = 1;
-		} else {
-			if (!(size_ < capacity_)) {
+		}
+		else
+		{
+			if (!(size_ < capacity_))
+			{
 				reserve(capacity_ * 2);
 			}
 			elem_[size_++] = value;
@@ -207,10 +237,13 @@ public:
 	}
 
 	//reserve "new_cap" amount of memory
-	void reserve(size_type new_cap) {
-		if (capacity_ < new_cap) {
+	void reserve(size_type new_cap)
+	{
+		if (capacity_ < new_cap)
+		{
 			value_type *tmp = new value_type[new_cap];
-			for (auto i = 0; i < size_; i++) {
+			for (auto i = 0; i < size_; i++)
+			{
 				tmp[i] = elem_[i];
 			}
 			delete[] elem_;
@@ -221,24 +254,35 @@ public:
 	}
 
 	//resizes the container
-	void resize(size_type count) {
-		if (count < 0) throw std::out_of_range {"Vector::resize"};
-		if (count < size_) {
+	void resize(size_type count)
+	{
+		if (count < 0)
+			throw std::out_of_range{"Vector::resize"};
+		if (count < size_)
+		{
 			pointer tmp = new value_type[count];
 			size_ = count;
-			for (auto i = 0; i < size_; i++) {
+			for (auto i = 0; i < size_; i++)
+			{
 				tmp[i] = elem_[i];
 			}
 			delete[] elem_;
 			elem_ = tmp;
 			tmp = nullptr;
-		} else if ((count > size_ && count <= capacity_) || count > capacity_) {
-			if (count > capacity_) { reserve(count); }
+		}
+		else if ((count > size_ && count <= capacity_) || count > capacity_)
+		{
+			if (count > capacity_)
+			{
+				reserve(count);
+			}
 			pointer tmp = new value_type[count];
-			for (auto i = 0; i < size_; i++) {
+			for (auto i = 0; i < size_; i++)
+			{
 				tmp[i] = elem_[i];
 			}
-			for ( auto i = size_; i < count; i++) {
+			for (auto i = size_; i < count; i++)
+			{
 				tmp[i] = value_type();
 			}
 			size_ = count;
@@ -252,10 +296,13 @@ public:
 	size_type size() const { return size_; }
 
 	//shrinks Vector capacity to size
-	void shrink_to_fit() {
-		if (size_ < capacity_) {
+	void shrink_to_fit()
+	{
+		if (size_ < capacity_)
+		{
 			value_type *tmp = new value_type[size_];
-			for (auto i = 0 ; i < size_; i++) {
+			for (auto i = 0; i < size_; i++)
+			{
 				tmp[i] = elem_[i];
 			}
 			delete[] elem_;
@@ -265,25 +312,32 @@ public:
 		}
 	}
 
-	void swap(Vector& other) {
+	void swap(Vector &other)
+	{
 		Vector tmp = *this;
 		*this = other;
 		other = tmp;
 	}
-//////////////////////OPERATORS//////////////////////
+	//////////////////////OPERATORS//////////////////////
 	//member access
-	reference operator[](size_type i) {
-		if (i < 0 || capacity() <= i) throw std::out_of_range {"Vector::operator[]"};
+	reference operator[](size_type i)
+	{
+		if (i < 0 || capacity() <= i)
+			throw std::out_of_range{"Vector::operator[]"};
 		return elem_[i];
 	}
 	//const memeber access
-	const_reference operator[](size_type i) const {
-		if (i < 0 || capacity() <= i) throw std::out_of_range {"Vector::operator[]_c"};
+	const_reference operator[](size_type i) const
+	{
+		if (i < 0 || capacity() <= i)
+			throw std::out_of_range{"Vector::operator[]_c"};
 		return elem_[i];
 	}
 	//coppy assignmment
-	Vector<value_type>& operator=(const Vector& vec) {
-		if (&vec == this) return *this;
+	Vector<value_type> &operator=(const Vector &vec)
+	{
+		if (&vec == this)
+			return *this;
 		pointer p = new value_type[vec.size_];
 		for (int i = 0; i != vec.size_; ++i)
 			p[i] = vec.elem_[i];
@@ -294,7 +348,8 @@ public:
 		return *this;
 	}
 	//move assignment
-	Vector<value_type>& operator=(Vector&& vec) {
+	Vector<value_type> &operator=(Vector &&vec)
+	{
 		if (&vec == this)
 			return *this;
 		delete[] elem_;
@@ -306,12 +361,12 @@ public:
 		vec.capacity_ = 0;
 		return *this;
 	}
+
 private:
 	pointer elem_;
 	size_type size_;
 	size_type capacity_;
 };
-
 
 #endif
 
---
+++ readme.md
@@ -0,0 +1,73 @@
+# Vector konteineris
+
+## 1 užduotis
+
+```c++
+void reserve(size_type new_cap) {
+    if (capacity_ < new_cap) {
+        value_type *tmp = new value_type[new_cap];
+        for (auto i = 0; i < size_; i++) {
+            tmp[i] = elem_[i];
+        }
+        delete[] elem_;
+        elem_ = tmp;
+        capacity_ = new_cap;
+        tmp = nullptr;
+    }
+}
+```
+Funkcija pakeičia konteinerio talpą jeigu pateiktas argumentas `new_cap` yra didesnis nei esama konteinerio talpa. Jei argumentas tenkina sąlygą, konteinerio elementai perkopijuojami į naujai išskirtą atminties vietą, o sena atminties vieta atlaisvinama.
+
+
+```c++
+void shrink_to_fit() {
+    if (size_ < capacity_) {
+        value_type *tmp = new value_type[size_];
+        for (auto i = 0 ; i < size_; i++) {
+            tmp[i] = elem_[i];
+        }
+        delete[] elem_;
+        elem_ = tmp;
+        tmp = nullptr;
+        capacity_ = size_;
+    }
+}
+```
+
+Funkcija sumažina konteinerio talpą iki tiek, kad nebūtų lasivos vietos konteineryje. Mažinimas vyksta perkopojuojant konteinerio elementus į naujai išskirtą atminties vietą; perkovijavus sena atmintis atlaisvinama ir konteinerio rodyklė nukreipiama į naujai išskirtą atminties vietą.
+
+```c++
+void push_back(const_reference value ) {
+    if (capacity_ == 0) {
+        elem_ = new value_type[1];
+        elem_[0] = value;
+        capacity_ = 1;
+        size_ = 1;
+    } else {
+        if (!(size_ < capacity_)) {
+            reserve(capacity_ * 2);
+        }
+        elem_[size_++] = value;
+    }
+}
+```
+
+Funkcija įterpia nurodytą elementą į konteinerio galą. Prieš įterpimą patikrinama ar konteinerio talpa nelygi nuliui ir ar konteinerio dydis mažesnis už talpą. Skirtingais atvėjais vykdomas skirtingas elemento įterpimas arba papildomas konteinerio dydžio pakeitimas.
+
+```c++
+void pop_back() {
+    if (size_ > 0) {
+        elem_[--size_].~value_type();
+    }
+}
+```
+Funkcija ištrina paskutinį konteinerio elementą iškviesdama jo destruktorių. Konteinerio talpa nepakinta. Dydis keičiasi.
+
+```c++
+reference operator[](size_type i)	{
+    if (i < 0 || capacity() <= i)
+        throw std::out_of_range{"Vector::operator[]"};
+    return elem_[i];
+}
+```
+Operatorius leidžia pasiekti bet kokį konteinerio elementą ir gražina "referenc'ą" į jį.
